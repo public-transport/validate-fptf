c93e4bdbed2ee9c23f24c5886862231971c95707
Jannis R <mail@jannisr.de>
2017-12-15 21:49:29 +0100

    validate agains FPTF@1.0.1

--- example.js
+++ example.js
@@ -1,37 +1,7 @@
 'use strict'
 
+const validJourney = require('friendly-public-transport-format/examples/valid-journey.json')
+
 const validate = require('.')
 
-validate({
-  type: 'journey',
-  id: '12345',
-  legs: [{
-    origin: {
-      type: 'station',
-      id: '12345678',
-      name: 'Foo',
-      location: {
-        type: 'location',
-        longitude: -20,
-        latitude: 30
-      }
-    },
-    destination: {
-      type: 'station',
-      id: '87654321',
-      name: 'Bar'
-    },
-    departure: '2017-03-16T20:00:00+01:00',
-    departurePlatform: '4-1',
-    arrival: '2017-03-17T15:00:00+02:00',
-    arrivalPlatform: '9',
-    schedule: '1234',
-    mode: 'walking',
-    public: true,
-    operator: 'sncf'
-  }],
-  price: {
-    amount: 19.95,
-    currency: 'EUR'
-  }
-})
+validate(validJourney)
---
+++ test.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const test = require('tape')
+const validJourney = require('friendly-public-transport-format/examples/valid-journey.json')
+const validSimpleJourney = require('friendly-public-transport-format/examples/valid-simple-journey.json')
+const invalidJourney = require('friendly-public-transport-format/examples/invalid-journey.json')
+
+const validateFptf = require('.')
+
+test('passes with valid-journey.json from FPTF', (t) => {
+  t.doesNotThrow(() => validateFptf(validJourney))
+  t.doesNotThrow(() => validateFptf(validSimpleJourney))
+  t.throws(() => validateFptf(invalidJourney))
+
+  t.end()
+})
+
+test('passes with valid-simple-journey.json from FPTF', (t) => {
+  t.doesNotThrow(() => validateFptf(validSimpleJourney))
+
+  t.end()
+})
+
+test('fails with invalid-journey.json from FPTF', (t) => {
+  t.throws(() => validateFptf(invalidJourney))
+
+  t.end()
+})
